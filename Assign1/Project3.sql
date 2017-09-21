4591e41673c7c846cf8befcefe6c16c43d165181
skrapmi <parkstimothj@csu.fullerton.edu>
2017-09-21 01:06:31 -0700

    added project 3

---
+++ Assign1/Project3.sql
@@ -0,0 +1,73 @@
+insert into department values ('Research',5,'333445555','1988-05-22');
+
+insert into department values ('Administration',4,'987654321','1995-01-01');
+
+insert into department values ('Headquarters',1,'888665555','1981-06-19');
+
+insert into dept_locations values (1,'Houston');
+
+insert into dept_locations values (4,'Stafford');
+
+insert into dept_locations values (5,'Bellaire');
+
+insert into dept_locations values (5,'Sugarland');
+
+insert into dept_locations values (5,'Houston');
+
+insert into works_on values('123456789',1,32.5);
+
+insert into works_on values('123456789',2,7.5);
+
+insert into works_on values('666884444',3,40.0);
+
+insert into works_on values('453453453',1,20.0);
+
+insert into works_on values('453453453',2,20.0);
+
+insert into works_on values('333445555',2,10.0);
+
+insert into works_on values('333445555',3,10.0);
+
+insert into works_on values('333445555',10,10.0);
+
+insert into works_on values('333445555',20,10.0);
+
+insert into works_on values('999887777',30,30.0);
+
+insert into works_on values('999887777',10,10.0);
+
+insert into works_on values('987987987',10,35.0);
+
+insert into works_on values('987987987',30,5.0);
+
+insert into works_on values('987654321',30,20.0);
+
+insert into works_on values('987654321',20,15.0);
+
+insert into works_on values('888665555',20,0.0);
+
+insert into project values('ProductX',1,'Bellaire',5);
+
+insert into project values('ProductY',2,'Sugarland',5);
+
+insert into project values('ProductZ',3,'Houston',5);
+
+insert into project values('Computerization',10,'Stafford',4);
+
+insert into project values('Reorganization',20,'Houston',1);
+
+insert into project values('Newbenefits',30,'Stafford',4);
+
+insert into dependent values('333445555','Alice','F','1986-04-05','Daughter');
+
+insert into dependent values('333445555','Theodore','M','1983-10-25','Son');
+
+insert into dependent values('333445555','Joy','F','1958-05-03','Spouse');
+
+insert into dependent values('987654321','Abner','M','1942-02-28','Spouse');
+
+insert into dependent values('123456789','Michael','M','1988-01-04','Son');
+
+insert into dependent values('123456789','Alice','F','1988-12-30','Daughter');
+
+insert into dependent values('123456789','Elizabeth','F','1987-05-05','Spouse');
