58ee7234faa2947896581096654be61cfec0a15f
SeaLabEZF <sean.wulwick@gmail.com>
2017-09-25 10:40:46 -0700

    Final Project2 commit
    Sorry for late commit, the error was you had to create the projects before the works_on, this ensure the Pno had a forgein key to reference.

--- Assign1/Project2.sql
+++ Assign1/Project2.sql
@@ -1,343 +1,344 @@
+
 insert into employee values (
-Fname(John),
-Minit(B), 
-Lname(Smith), 
-ssn(123456789), 
-Bdate(1965-01-09), 
-Address(731Fondren, Houston, TX),
-sex(M),
-Salary(30000),
-Super_ssn(333445555),
-Dno(5)
+'John',
+'B', 
+'Smith', 
+'123456789', 
+'1965-01-09', 
+'731 Fondren, Houston, TX',
+'M',
+30000,
+'333445555',
+5
 );
 
 insert into employee values (
-Fname(Franklin),
-Minit(T), 
-Lname(Wong), 
-ssn(333445555), 
-Bdate(1955-12-08), 
-Address(638 Voss, Houston, TX),
-sex(M),
-Salary(40000),
-Super_ssn(888665555),
-Dno(5)
+'Franklin',
+'T', 
+'Wong', 
+'333445555', 
+'1955-12-08', 
+'638 Voss, Houston, TX',
+'M',
+40000,
+'888665555',
+5
 );
 
 insert into employee values (
-Fname(Alicia),
-Minit(T), 
-Lname(Zelaya), 
-ssn(999887777), 
-Bdate(1968-01-19), 
-Address(3321 Castle, Spring, TX),
-sex(F),
-Salary(25000),
-Super_ssn(987654321),
-Dno(4)
+'Alicia',
+'T', 
+'Zelaya', 
+'999887777', 
+'1968-01-19', 
+'3321 Castle, Spring, TX',
+'F',
+25000,
+'987654321',
+4
 );
 
 insert into employee values (
-Fname(Jennifer),
-Minit(S), 
-Lname(Wallace), 
-ssn(987654321), 
-Bdate(1941-06-20), 
-Address(291 Berry, Bellaire, TX),
-sex(F),
-Salary(43000),
-Super_ssn(888665555),
-Dno(4)
+'Jennifer',
+'S', 
+'Wallace', 
+'987654321', 
+'1941-06-20', 
+'291 Berry, Bellaire, TX',
+'F',
+43000,
+'888665555',
+4
 );
 
 insert into employee values (
-Fname(Ramesh),
-Minit(K), 
-Lname(Narayan), 
-ssn(666884444), 
-Bdate(1962-09-15), 
-Address(975 Fire_Oak, Humble, TX),
-sex(M),
-Salary(38000),
-Super_ssn(333445555),
-Dno(5)
+'Ramesh',
+'K', 
+'Narayan', 
+'666884444', 
+'1962-09-15', 
+'975 Fire_Oak, Humble, TX',
+'M',
+38000,
+'333445555',
+5
 );
 
 insert into employee values (
-Fname(Joyce),
-Minit(A), 
-Lname(English), 
-ssn(453453453), 
-Bdate(1972-07-31), 
-Address(5631 Rice, Houston, TX),
-sex(F),
-Salary(25000),
-Super_ssn(333445555),
-Dno(5)
+'Joyce',
+'A', 
+'English', 
+'453453453', 
+'1972-07-31', 
+'5631 Rice, Houston, TX',
+'F',
+25000,
+'333445555',
+5
 );
 
 insert into employee values (
-Fname(Ahmad),
-Minit(V), 
-Lname(Jabbar), 
-ssn(987987987), 
-Bdate(1969-03-29), 
-Address(980 Dallas, Houston, TX),
-sex(M),
-Salary(25000),
-Super_ssn(987654321),
-Dno(4)
+'Ahmad',
+'V', 
+'Jabbar', 
+'987987987', 
+'1969-03-29', 
+'980 Dallas, Houston, TX',
+'M',
+25000,
+'987654321',
+4
 );
 
 insert into employee values (
-Fname(James),
-Minit(E), 
-Lname(Borg), 
-ssn(888665555), 
-Bdate(1937-11-10), 
-Address(450 Stone, Houston, TX),
-sex(M),
-Salary(55000),
-Super_ssn(NULL),
-Dno(1)
+'James',
+'E', 
+'Borg', 
+'888665555', 
+'1937-11-10', 
+'450 Stone, Houston, TX',
+'M',
+55000,
+NULL,
+1
 );
 
 insert into department values (
-Dname(Research),
-Dnumber(5),
-Mgr_ssn(333445555),
-Mgr_start_date(1988-05-22)
+'Research',
+5,
+'333445555',
+'1988-05-22'
 );
 
 insert into department values (
-Dname(Administration),
-Dnumber(4),
-Mgr_ssn(987654321),
-Mgr_start_date(1995-01-01)
+'Administration',
+4,
+'987654321',
+'1995-01-01'
 );
 
 insert into department values (
-Dname(Headquarters),
-Dnumber(1),
-Mgr_ssn(888665555),
-Mgr_start_date(1981-06-19)
+'Headquarters',
+1,
+'888665555',
+'1981-06-19'
 );
 
 insert into dept_locations values (
-Dnumber(1),
-Dlocation(Houston)
+1,
+'Houston'
 );
 
 insert into dept_locations values (
-Dnumber(4),
-Dlocation(Stafford)
+4,
+'Stafford'
 );
 
 insert into dept_locations values (
-Dnumber(5),
-Dlocation(Bellaire)
+5,
+'Bellaire'
 );
 
 insert into dept_locations values (
-Dnumber(5),
-Dlocation(Sugarland)
+5,
+'Sugarland'
 );
 
 insert into dept_locations values (
-Dnumber(5),
-Dlocation(Houston)
-);
-
-insert into works_on values(
-Essn(123456789),
-Pno(1),
-Hours(32.5)
-);
-
-insert into works_on values(
-Essn(123456789),
-Pno(2),
-Hours(7.5)
-);
-
-insert into works_on values(
-Essn(666884444),
-Pno(3),
-Hours(40.0)
-);
-
-insert into works_on values(
-Essn(453453453),
-Pno(1),
-Hours(20.0)
-);
-
-insert into works_on values(
-Essn(453453453),
-Pno(2),
-Hours(20.0)
-);
-
-insert into works_on values(
-Essn(333445555),
-Pno(2),
-Hours(10.0)
-);
-
-insert into works_on values(
-Essn(333445555),
-Pno(3),
-Hours(10.0)
-);
-
-insert into works_on values(
-Essn(333445555),
-Pno(10),
-Hours(10.0)
-);
-
-insert into works_on values(
-Essn(333445555),
-Pno(20),
-Hours(10.0)
-);
-
-insert into works_on values(
-Essn(999887777),
-Pno(30),
-Hours(30.0)
-);
-
-insert into works_on values(
-Essn(999887777),
-Pno(10),
-Hours(10.0)
-);
-
-insert into works_on values(
-Essn(987987987),
-Pno(10),
-Hours(35.0)
-);
-
-insert into works_on values(
-Essn(987987987),
-Pno(30),
-Hours(5.0)
-);
-
-insert into works_on values(
-Essn(987654321),
-Pno(30),
-Hours(20.0)
-);
-
-insert into works_on values(
-Essn(987654321),
-Pno(20),
-Hours(15.0)
-);
-
-insert into works_on values(
-Essn(888665555),
-Pno(20),
-Hours(0.0)
+5,
+'Houston'
 );
 
 insert into project values(
-Pname(ProductX),
-Pnumber(1),
-Plocation(Bellaire),
-Dnum(5)
+'ProductX',
+1,
+'Bellaire',
+5
 );
 
 insert into project values(
-Pname(ProductY),
-Pnumber(2),
-Plocation(Sugarland),
-Dnum(5)
+'ProductY',
+2,
+'Sugarland',
+5
 );
 
 insert into project values(
-Pname(ProductZ),
-Pnumber(3),
-Plocation(Houston),
-Dnum(5)
+'ProductZ',
+3,
+'Houston',
+5
 );
 
 insert into project values(
-Pname(Computerization),
-Pnumber(10),
-Plocation(Stafford),
-Dnum(4)
+'Computerization',
+10,
+'Stafford',
+4
 );
 
 insert into project values(
-Pname(Reorganization),
-Pnumber(20),
-Plocation(Houston),
-Dnum(1)
+'Reorganization',
+20,
+'Houston',
+1
 );
 
 insert into project values(
-Pname(Newbenefits),
-Pnumber(30),
-Plocation(Stafford),
-Dnum(4)
+'Newbenefits',
+30,
+'Stafford',
+4
+);
+
+insert into works_on values(
+'123456789',
+1,
+32.5
+);
+
+insert into works_on values(
+'123456789',
+2,
+7.5
+);
+
+insert into works_on values(
+'666884444',
+3,
+40.0
+);
+
+insert into works_on values(
+'453453453',
+1,
+20.0
+);
+
+insert into works_on values(
+'453453453',
+2,
+20.0
+);
+
+insert into works_on values(
+'333445555',
+2,
+10.0
+);
+
+insert into works_on values(
+'333445555',
+3,
+10.0
+);
+
+insert into works_on values(
+'333445555',
+10,
+10.0
+);
+
+insert into works_on values(
+'333445555',
+20,
+10.0
+);
+
+insert into works_on values(
+'999887777',
+30,
+30.0
+);
+
+insert into works_on values(
+'999887777',
+10,
+10.0
+);
+
+insert into works_on values(
+'987987987',
+10,
+35.0
+);
+
+insert into works_on values(
+'987987987',
+30,
+5.0
+);
+
+insert into works_on values(
+'987654321',
+30,
+20.0
+);
+
+insert into works_on values(
+'987654321',
+20,
+15.0
+);
+
+insert into works_on values(
+'888665555',
+20,
+0.0
 );
 
 insert into dependent values(
-Ess(333445555),
-Dependent_name(Alice),
-Sex(F),
-Bdate(1986-04-05),
-Relationship(Daughter)
+'333445555',
+'Alice',
+'F',
+'1986-04-05',
+'Daughter'
 );
 
 insert into dependent values(
-Ess(333445555),
-Dependent_name(Theodore),
-Sex(M),
-Bdate(1983-10-25),
-Relationship(Son)
+'333445555',
+'Theodore',
+'M',
+'1983-10-25',
+'Son'
 );
 
 insert into dependent values(
-Ess(333445555),
-Dependent_name(Joy),
-Sex(F),
-Bdate(1958-05-03),
-Relationship(Spouse)
+'333445555',
+'Joy',
+'F',
+'1958-05-03',
+'Spouse'
 );
 
 insert into dependent values(
-Ess(987654321),
-Dependent_name(Abner),
-Sex(M),
-Bdate(1942-02-28),
-Relationship(Spouse)
+'987654321',
+'Abner',
+'M',
+'1942-02-28',
+'Spouse'
 );
 
 insert into dependent values(
-Ess(123456789),
-Dependent_name(Michael),
-Sex(M),
-Bdate(1988-01-04),
-Relationship(Son)
+'123456789',
+'Michael',
+'M',
+'1988-01-04',
+'Son'
 );
 
 insert into dependent values(
-Ess(123456789),
-Dependent_name(Alice),
-Sex(F),
-Bdate(1988-12-30),
-Relationship(Daughter)
+'123456789',
+'Alice',
+'F',
+'1988-12-30',
+'Daughter'
 );
 
 insert into dependent values(
-Ess(123456789),
-Dependent_name(Elizabeth),
-Sex(F),
-Bdate(1987-05-05),
-Relationship(Spouse)
+'123456789',
+'Elizabeth',
+'F',
+'1987-05-05',
+'Spouse'
 );
